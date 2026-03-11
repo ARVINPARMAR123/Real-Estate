13912fcbad0a4ae18b144e06fffaf368166ff622
Arvin Parmar <arvinp498@gmail.com>
2026-03-11 19:23:11 +0530

    Update in Schema

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/", getPosts);
 router.get("/:id", getPost);
-router.post("/", verifyToken, addPost);
+router.post("/add", verifyToken, addPost);
 router.put("/:id", verifyToken, updatePost);
 router.delete("/:id", verifyToken, deletePost);
 
